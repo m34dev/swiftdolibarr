cf331b38e9ab7a59304f9cde279c3e0879177575
William Mead <william@m34d.com>
2026-04-09 14:06:41 +0200

    Added Dolibarr status and Dolibarr agenda event type. Updated CHANGELOG.

---
+++ Sources/SwiftDolibarr/Models/Core/Setup/DolibarrAgendaEventType.swift
@@ -0,0 +1,110 @@
+// Copyright 2026 M34D - William Mead
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//  DolibarrAgendaEventType.swift
+//  SwiftDolibarr
+//
+//  Created by William Mead on 17/06/2025.
+//
+
+import Foundation
+#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(visionOS)
+import OSLog
+#endif
+
+/// A Dolibarr agenda event type.
+///
+/// Maps to the Dolibarr `/setup/dictionary/event_types` REST API
+/// endpoint. Each type has a ``code``, and a ``label``
+///
+/// - Note: Requires the **Agenda** module to be activated in Dolibarr.
+/// - SeeAlso: ``DolibarrAgendaEvent``
+public struct DolibarrAgendaEventType: Hashable, Decodable, Sendable, DolibarrObject {
+
+    // MARK: - Properties
+
+    // Required
+
+    /// Agenda event type ID.
+    public var id: String
+
+    /// Agenda event type code
+    public var code: String
+
+    /// The event type classification (e.g. `"system"` or `"systemauto"`).
+    public var type: String
+
+    /// Display label for the agenda event type.
+    public var label: String
+
+    // Optional
+
+    /// The Dolibarr module this event type belongs to, if any.
+    public var module: String?
+
+    // MARK: - Enums
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case code
+        case type
+        case label
+        case module
+    }
+
+    // MARK: - Inits
+
+    public init(
+        id: String = "",
+        code: String = "",
+        type: String = "",
+        label: String = "",
+        module: String? = nil,
+    ) {
+        self.id = id
+        self.code = code
+        self.type = type
+        self.label = label
+        self.module = module
+    }
+
+    public init(from decoder: any Decoder) throws {
+        do {
+            #if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(visionOS)
+            Logger.logWithoutSignal("\(Self.self).init.decode", category: .api)
+            #endif
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.id = try container.decode(String.self, forKey: .id)
+            self.code = try container.decode(String.self, forKey: .code)
+            self.type = try container.decode(String.self, forKey: .type)
+            self.label = try container.decode(String.self, forKey: .label)
+            self.module = try container.decodeIfPresent(String.self, forKey: .module)
+            #if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(visionOS)
+            Logger.logWithoutSignal("\(Self.self).init.decoded", category: .api)
+            #endif
+        } catch let error as DecodingError {
+            #if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(visionOS)
+            Logger.logDecodingError(error, decodeContext: "\(Self.self).init")
+            #endif
+            throw error
+        } catch {
+            #if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(visionOS)
+            Logger.logErrorWithSignal(error, context: "\(Self.self).init", category: .api)
+            #endif
+            throw error
+        }
+    }
+
+}
